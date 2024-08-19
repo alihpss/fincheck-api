830a1c4527be0cec3e2046eb782ba4cd007ae6e0
alihpss <ali.hps@hotmail.com>
2024-08-19 09:06:48 -0300

    :construction: WIP: Categories controller

---
+++ src/modules/categories/categories.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { ActiveUserId } from 'src/shared/decorators/activeUserId';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll(@ActiveUserId() userId: string) {
+    return this.categoriesService.findAllByUserId(userId);
+  }
+
+  // @Post()
+  // create(@Body() createCategoryDto: CreateCategoryDto) {
+  //   return this.categoriesService.create(createCategoryDto);
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.categoriesService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateCategoryDto: UpdateCategoryDto,
+  // ) {
+  //   return this.categoriesService.update(+id, updateCategoryDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.categoriesService.remove(+id);
+  // }
+}
