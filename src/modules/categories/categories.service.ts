83116ef94e3271ba3dd400d94c6fc10b63cd9a27
alihpss <ali.hps@hotmail.com>
2024-08-19 09:07:37 -0300

    :sparkles: Feat: Add categories module, service, repository and dto´s

---
+++ src/modules/categories/categories.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriesRepository } from 'src/shared/database/repositories/categories.repositories copy';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly categoriesRepo: CategoriesRepository) {}
+
+  findAllByUserId(userId: string) {
+    return this.categoriesRepo.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  // create(createCategoryDto: CreateCategoryDto) {
+  //   return 'This action adds a new category';
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} category`;
+  // }
+
+  // update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  //   return `This action updates a #${id} category`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} category`;
+  // }
+}
